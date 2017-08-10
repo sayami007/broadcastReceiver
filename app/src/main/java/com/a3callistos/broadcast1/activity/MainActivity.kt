0e8ebe3948b32e6fdc438056d22146f65ac82f5e
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-08-10 13:14:53 +0545

    Sticky Broadcast

--- app/src/main/java/com/a3callistos/broadcast1/activity/MainActivity.kt
+++ app/src/main/java/com/a3callistos/broadcast1/activity/MainActivity.kt
@@ -1,37 +1,32 @@
 package com.a3callistos.broadcast1.activity
 
+import android.content.Intent
 import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.a3callistos.broadcast1.R
-import com.a3callistos.broadcast1.broadcast.MyFirstReceiver
 
 /**
  * Created by Bibesh on 8/7/17.
  */
 class MainActivity : AppCompatActivity() {
-    companion object {
-        val TAG: String = MainActivity::class.java.simpleName
-    }
 
-    var myFirstReceiver: MyFirstReceiver? = null
-
+    open val TAG: String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myFirstReceiver = MyFirstReceiver()
     }
 
-    override fun onResume() {
-        super.onResume()
+    fun methodOne(v: View) {
         var intentFilter: IntentFilter = IntentFilter()
-        intentFilter.addAction("android.intent.action.AIRPLANE_MODE")
-        registerReceiver(myFirstReceiver, intentFilter)
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED)
+
+        var intent: Intent = registerReceiver(null, intentFilter)
+        var status: Int = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+        println(status)
     }
 
-    override fun onPause() {
-        super.onPause()
-        unregisterReceiver(myFirstReceiver)
-    }
 }
